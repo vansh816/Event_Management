7d91a1e7a89adbe3914ee20180c6ba1202d7cbc3
Vansh816 <sharmavn258@email.com>
2026-03-28 18:47:08 +0530

    first commit

--- src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -1,13 +1,13 @@
-package net.engineeringdigest.journalApp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JournalAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package net.engineeringdigest.journalApp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JournalAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
