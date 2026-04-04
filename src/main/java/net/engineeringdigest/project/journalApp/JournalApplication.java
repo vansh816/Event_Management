b47fdabf0c67c7adecdd4d28c0f0011c32e1eb6c
Irfan-Hussain107 <irfanhussains98786@gmail.com>
2026-04-05 00:21:22 +0530

    fix

--- src/main/java/net/engineeringdigest/project/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/project/journalApp/JournalApplication.java
@@ -2,12 +2,14 @@ package net.engineeringdigest.journalApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "net.engineeringdigest.journalApp",
         "net.engineeringdigest.project.journalApp"
 })
+@EnableMongoRepositories(basePackages = "net.engineeringdigest.project.journalApp")
 public class JournalApplication {
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
